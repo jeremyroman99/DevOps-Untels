d8fc21febdef4c9274e37039e16256b650fb28bd
Carlos Cosme <danielcosme99@gmail.com>
2024-04-09 16:36:18 -0500

    Agregando layout de colaboradores

--- app/src/main/java/com/untels/devops/MainActivity.kt
+++ app/src/main/java/com/untels/devops/MainActivity.kt
@@ -1,46 +1,70 @@
 package com.untels.devops
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.untels.devops.ui.theme.DevOpsUntelsTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DevOpsUntelsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main_activity)
+
     }
-}
+    fun openLinkedIn1(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.linkedin.com/in/jeremyroman99/"
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
+    }
+    fun openLinkedIn2(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.linkedin.com/in/carloscosme99/"
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DevOpsUntelsTheme {
-        Greeting("Android")
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
+    }
+    fun openyoutube1(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
+
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
+    }
+    fun openyoutube2(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
+
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
+    }
+    fun openinstagram1(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.instagram.com/jeremy.roman99/"
+
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
+    }
+    fun openinstagram2(view: View?) {
+        // Aquí define la URL de LinkedIn que quieres abrir
+        val url = "https://www.instagram.com/carlosc.0199/"
+
+        // Crea un intent implícito para abrir el navegador con la URL especificada
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
     }
 }
