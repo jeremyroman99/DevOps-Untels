d4bbe614d16641edcc9cbc75e643d64e15bab554
Carlos Cosme <danielcosme99@gmail.com>
2024-04-16 01:28:24 -0500

    cambiando icon recursos

--- app/src/main/java/com/untels/devops/NavigationItem.kt
+++ app/src/main/java/com/untels/devops/NavigationItem.kt
@@ -2,6 +2,6 @@ package com.untels.devops
 
 sealed class NavigationItem(var route: String, var icon: Int, var title: String) {
     object ScreenAo : NavigationItem("a", R.drawable.ic_home, "Inicio")
-    object ScreenBo : NavigationItem("b", R.drawable.ic_search, "Recursos")
+    object ScreenBo : NavigationItem("b", R.drawable.ic_resource, "Recursos")
     object ScreenCo : NavigationItem("c", R.drawable.ic_collaborator, "Colaboradores")
 }
