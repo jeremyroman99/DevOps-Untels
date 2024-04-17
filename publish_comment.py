78fa9e510b7fd52056a9942d90150e30d349124d
Jeremy Roman <jeremyjor299@gmail.com>
2024-04-16 22:35:11 -0500

    fix: yml ci

--- publish_comment.py
+++ publish_comment.py
@@ -1,5 +1,6 @@
-from github import Github
+import json
 import os
+from github import Github
 
 # Obtener el token de GitHub
 github_token = os.environ['GITHUB_TOKEN']
@@ -7,9 +8,14 @@ github_token = os.environ['GITHUB_TOKEN']
 # Crear una instancia de la clase Github
 g = Github(github_token)
 
-# Obtener el contexto del repositorio y el número del pull request
-repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-pr_number = os.environ['GITHUB_EVENT_NUMBER']
+# Obtener el número del pull request desde el archivo de evento
+with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
+    event_data = json.load(f)
+    pr_number = event_data['pull_request']['number']
+
+# Obtener el contexto del repositorio
+repository = os.environ['GITHUB_REPOSITORY']
+repo = g.get_repo(repository)
 
 # Leer el contenido del archivo
 with open('formatted_overview.txt', 'r') as file:
@@ -21,3 +27,4 @@ comment_body = f"Datos de revisión formateados:\n```\n{content}\n```"
 # Publicar el comentario en el pull request
 pr = repo.get_pull(int(pr_number))
 pr.create_issue_comment(comment_body)
+
