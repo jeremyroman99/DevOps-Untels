e26263516b9bf5bb7eca269a5e1e8f2d4b2ca790
Jeremy Roman <jeremyjor299@gmail.com>
2024-04-19 00:09:04 -0500

    fix. content

--- publish_comment.py
+++ publish_comment.py
@@ -17,48 +17,15 @@ with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
 repository = os.environ['GITHUB_REPOSITORY']
 repo = g.get_repo(repository)
 
-# Leer el contenido del archivo JSON
-with open('formatted_table.json', 'r') as file:
-    table_data = json.load(file)
+# Leer el contenido del archivo formatted_overview.txt
+with open('formatted_overview.txt', 'r') as file:
+    lines = file.readlines()
 
-# Construir la tabla Markdown a partir de los datos del archivo JSON
-table_content = "| First Header | Second Header |\n| ------------- | ------------- |\n"
-for item in table_data:
-    table_content += f"| {item['First Header']} | {item['Second Header']} |\n"
+# Construir el contenido de la tabla a partir de las líneas del archivo
+table_content = ''.join(lines)
 
 # Crear el cuerpo del comentario con el contenido de la tabla
-comment_body = f"```
-                {
-                "type": "Topology",
-                "transform": {
-                    "scale": [0.0005000500050005, 0.00010001000100010001],
-                    "translate": [100, 0]},
-                "objects": {
-                    "example": {
-                    "type": "GeometryCollection",
-                    "geometries": [
-                        {
-                        "type": "Point",
-                        "properties": {"prop0": "value0"},
-                        "coordinates": [4000, 5000]
-                        },
-                        {
-                        "type": "LineString",
-                        "properties": {"prop0": "value0", "prop1": 0},
-                        "arcs": [0]
-                        },
-                        {
-                        "type": "Polygon",
-                        "properties": {"prop0": "value0",
-                            "prop1": {"this": "that"}
-                        },
-                        "arcs": [[1]]
-                        }
-                    ]
-                    }
-                },
-                "arcs": [[[4000, 0], [1999, 9999], [2000, -9999], [2000, 9999]],[[0, 0], [0, 9999], [2000, 0], [0, -9999], [-2000, 0]]]
-                }```"
+comment_body = f"```\n{table_content}\n```"
 
 # Publicar el contenido del archivo como un comentario en el pull request
 pr = repo.get_pull(int(pr_number))
