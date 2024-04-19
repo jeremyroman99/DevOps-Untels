b808aa1fb8b8c90c2353e2e0a4f317850be31976
Jeremy Roman <jeremyjor299@gmail.com>
2024-04-19 00:03:19 -0500

    fix. content

--- publish_comment.py
+++ publish_comment.py
@@ -27,7 +27,38 @@ for item in table_data:
     table_content += f"| {item['First Header']} | {item['Second Header']} |\n"
 
 # Crear el cuerpo del comentario con el contenido de la tabla
-comment_body = f"```\n{table_content}\n```"
+comment_body = f"```
+                {
+                "type": "Topology",
+                "transform": {
+                    "scale": [0.0005000500050005, 0.00010001000100010001],
+                    "translate": [100, 0]},
+                "objects": {
+                    "example": {
+                    "type": "GeometryCollection",
+                    "geometries": [
+                        {
+                        "type": "Point",
+                        "properties": {"prop0": "value0"},
+                        "coordinates": [4000, 5000]
+                        },
+                        {
+                        "type": "LineString",
+                        "properties": {"prop0": "value0", "prop1": 0},
+                        "arcs": [0]
+                        },
+                        {
+                        "type": "Polygon",
+                        "properties": {"prop0": "value0",
+                            "prop1": {"this": "that"}
+                        },
+                        "arcs": [[1]]
+                        }
+                    ]
+                    }
+                },
+                "arcs": [[[4000, 0], [1999, 9999], [2000, -9999], [2000, 9999]],[[0, 0], [0, 9999], [2000, 0], [0, -9999], [-2000, 0]]]
+                }```"
 
 # Publicar el contenido del archivo como un comentario en el pull request
 pr = repo.get_pull(int(pr_number))
